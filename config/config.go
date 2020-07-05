69af64af4061a44ccae72b49da27220b747492d6
Wako <dulo.rodrigo@gmail.com>
2020-07-05 04:07:03 -0500

    It uses a .envfile to read the user details
    It creates a Settings struct with details about MPD connection such as
    port, server, database of scrobbler, username and password of last.fm
    
    Moves the implementation to the main package

---
+++ config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+// config file
+// database file
+// mpd port
+// mpd server ?
+
+// Settings contains the information to succesfuly config a
+// Last.fm user for MPD
+type Settings struct {
+	Database string
+	Port     int
+	Server   string
+	User     User
+}
+
+// User contains username and password for
+// your Last.fm account
+type User struct {
+	Username string
+	Password string
+}
+
+// New returns a pointer to a settings struct
+func New() *Settings {
+	return &Settings{
+		Database: getEnvAsString("DATABASE", "./scrobble.db"),
+		Port:     getEnvAsInt("PORT", 6600),
+		Server:   getEnvAsString("SERVER", "localhost"),
+		User: User{
+			Username: getEnvAsString("USERNAME", ""),
+			Password: getEnvAsString("PASSWORD", ""),
+		},
+	}
+}
+
+// getEnv returns the value of a environment variable or a default value as a string
+func getEnvAsString(key string, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+// getEnv returns the value of a environment variable or a default value as a int
+func getEnvAsInt(key string, defaultValue int) int {
+	valueString := getEnvAsString(key, "")
+	if value, err := strconv.Atoi(valueString); err == nil {
+		return value
+	}
+	return defaultValue
+}
