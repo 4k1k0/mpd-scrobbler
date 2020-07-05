5d682ba2b1c6154a77a1a55955562a3fa86f7305
Wako <dulo.rodrigo@gmail.com>
2020-07-05 04:35:38 -0500

    It finish the implementation
    This finish the implementation of the current code as far in the app
    module
    
    Next step will be edit the scrobble package to handle Last.fm as a
    default or only option

--- app/run.go
+++ app/run.go
@@ -3,11 +3,34 @@ package app
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/joho/godotenv"
 	"hawx.me/code/mpd-scrobbler/client"
 	"hawx.me/code/mpd-scrobbler/config"
+	"hawx.me/code/mpd-scrobbler/scrobble"
 )
+
+const (
+	// only submit tracks longer then minTrackLen
+	minTrackLen = 30
+
+	// only submit if played for submitTime second or submitPercentage of length
+	submitTime       = 240
+	submitPercentage = 50
+
+	// polling interval
+	sleepTime = 5 * time.Second
+)
+
+func catchInterrupt() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	s := <-c
+	log.Printf("caught %s: shutting down", s)
+}
 
 // StartApp runs the program this way the main package is clean
 func StartApp() {
@@ -25,4 +48,43 @@ func StartApp() {
 	}
 	// Close the conection at the end of the main goroutine
 	defer c.Close()
+
+	// It creates a database connection?
+	// and defer the database close
+	db, err := scrobble.Open(settings.Database)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// TODO: Handle this hardcoded strings
+	api, err := scrobble.New(db, "", "", "", settings.User.Username, settings.User.Password, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// This channels stores the song to submit and the now playing values
+	toSubmit := make(chan client.Song)
+	nowPlaying := make(chan client.Song)
+
+	go c.Watch(sleepTime, toSubmit, nowPlaying)
+
+	go func() {
+		for {
+			select {
+			case s := <-nowPlaying:
+				err := api.NowPlaying(s.Artist, s.Album, s.AlbumArtist, s.Title)
+				if err != nil {
+					log.Printf("[%s] err(NowPlaying): %s\n", api.Name(), err)
+				}
+			case s := <-toSubmit:
+				err := api.Scrobble(s.Artist, s.Album, s.AlbumArtist, s.Title, s.Start)
+				if err != nil {
+					log.Printf("[%s] err(Scrobble): %s\n", api.Name(), err)
+				}
+			}
+		}
+	}()
+
+	catchInterrupt()
 }
